75fecc2d434d3cecbeb6948f81e4dc273e8170db
smruti901 <109574733+smruti901@users.noreply.github.com>
2022-07-19 17:02:46 +0530

    Update Newclass.java
    asda

--- NewClass/src/test/java/New/Newclass.java
+++ NewClass/src/test/java/New/Newclass.java
@@ -8,6 +8,7 @@ public class Newclass {
 	public void stat() {
 
 		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");System.out.println("New vgffNMbajJ");System.out.println("New vgffNMbajJ");System.out.println("New vgffNMbajJ");
 
 	}
 
