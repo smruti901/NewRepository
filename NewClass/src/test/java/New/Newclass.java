64cb667c2107a283df2556d4cdde2f0c6f107710
DELL <DELL@DESKTOP-FUG0H6V>
2022-07-19 17:48:24 +0530

    asjdlkajsjdald

--- NewClass/src/test/java/New/Newclass.java
+++ NewClass/src/test/java/New/Newclass.java
@@ -8,11 +8,8 @@ public class Newclass {
 	public void stat() {
 
 		System.out.println("New vgffNMbajJ");
-		System.out.println("New vgffNMbajJ");
-		System.out.println("New vgffNMbajJ");
-		System.out.println("New vgffNMbajJ");
-		System.out.println("New vgffNMbajJ");
-		System.out.println("New vgffNMbajJ");
+		
+		
 
 	}
 
