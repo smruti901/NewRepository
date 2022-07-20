8429a1490832d0b0990d3a2c0a0f53c3fa2411bd
DELL <DELL@DESKTOP-FUG0H6V>
2022-07-20 11:19:04 +0530

    Todays's update

--- NewClass/src/test/java/New/Newclass.java
+++ NewClass/src/test/java/New/Newclass.java
@@ -14,6 +14,21 @@ public class Newclass {
 		System.out.println("New vgffNMbajJ");
 		System.out.println("New vgffNMbajJ");
 		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		System.out.println("New vgffNMbajJ");
+		
 		
 		
 
