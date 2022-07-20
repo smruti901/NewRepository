abb1906646c30f89b53cf5f5e1b9743ada2543a8
DELL <DELL@DESKTOP-FUG0H6V>
2022-07-20 16:27:44 +0530

    New build

--- NewClass/src/test/java/New/Newclass.java
+++ NewClass/src/test/java/New/Newclass.java
@@ -6,6 +6,9 @@ public class Newclass {
 
 	@Test
 	public void stat() {
+		
+		String browser= System.getProperty("browser");
+		String url=System.getProperty("url");
 
 		System.out.println("New vgffNMbajJ");
 		System.out.println("New vgffNMbajJ");
